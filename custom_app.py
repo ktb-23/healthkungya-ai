b1f5f8c95e8b8baeb8cd22c75400c69e581eb433
PokingTeemo <yoonsub1234@gmail.com>
2024-09-11 09:30:58 +0900

    Update custom_app.py

--- custom_app.py
+++ custom_app.py
@@ -1,102 +1,107 @@
-# 가상환경 설정 python -m venv venv
-# 가상환경 실행 activate (또는 source venv/bin/activate)
-# python custom_app.py 입력
-# 가상환경 종료 deactivate
+# 가상환경 생성: python -m venv venv
+# 가상환경 실행: activate (또는 source venv/bin/activate)
+# 필요한 모듈 설치: pip install -r requirements.txt
+# 서버 실행: flask --app custom_app run --port 5001
+# 가상환경 종료: deactivate
 
-# 필요한 모듈 설치해주세요. (flask_cors 등)
-
-import os
-from flask import Flask, request, jsonify, session, send_from_directory
+from flask import Flask, request, jsonify
 import pandas as pd
-from azure.cognitiveservices.vision.customvision.prediction import CustomVisionPredictionClient
+from azure.cognitiveservices.vision.customvision.prediction import (
+    CustomVisionPredictionClient,
+)
 from msrest.authentication import ApiKeyCredentials
-from flask_cors import CORS
-from dotenv import load_dotenv
-
-load_dotenv()  # .env 파일에서 환경 변수를 불러옴
-training_key = os.getenv('VISION_TRAINING_KEY')
-training_endpoint = os.getenv('VISION_TRAINING_ENDPOINT')
+import requests
+import threading
 
 app = Flask(__name__)
-app.secret_key = 'a1B2c3D4e5F6g7H8i9J0kLmNoPqRsTuVwXyZ' # 세션을 사용하기 위해 필요
-
-# CORS 설정
-# CORS(app, resources={r"/*": {"origins": "https://apis.doongyi.com"}}, supports_credentials=True)
-# 만약 모든 origine을 허용한다면 위 코드 대신 아래의 코드 넣기
-CORS(app, supports_credentials=True)
+app.secret_key = "a1B2c3D4e5F6g7H8i9J0kLmNoPqRsTuVwXyZ"  # 세션을 사용하기 위해 필요
 
 # Custom Vision 설정
 prediction_endpoint = "https://healthkungyacv-prediction.cognitiveservices.azure.com/"
+# XXX: 키가 코드에 직접 들어가 있습니다. 환경변수로 옮기는 것이 좋습니다.
 prediction_key = "7c0683360f1e4e6f937f3c3fa111f233"
 
 # DB 파일 읽어오기
 food_db = pd.read_excel("Food_DB.xlsx")
+food_db["음 식 명"] = food_db["음 식 명"].str.strip()
 
 # Custom Vision Prediction 클라이언트 초기화
-prediction_credentials = ApiKeyCredentials(in_headers={"Prediction-key": prediction_key})
+prediction_credentials = ApiKeyCredentials(
+    in_headers={"Prediction-key": prediction_key}
+)
 predictor = CustomVisionPredictionClient(prediction_endpoint, prediction_credentials)
 
 # 프로젝트 ID와 퍼블리시 설정
 project_id = "56bddca9-b0ee-45b6-aa4b-064b5a618619"
 publish_iteration_name = "Iteration3"
 
-@app.route('/favicon.ico')
-def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static'),
-                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
-@app.route('/', methods=['GET'])
-def home():
-    return "Welcome to the Custom Vision Prediction API!", 200
+def get_kcal_of_tag_in_db(tag):
+    tag = tag.strip()
 
-@app.route('/predict', methods=['GET'])
-def get_image_url():
-    # 이미지 URL을 GET 요청 / 풀스택분들이 전달해주는 image_url
-    image_url = request.args.get('image_url')
+    matching_row = food_db[food_db["음 식 명"] == tag]
 
-    if not image_url:
-        return jsonify({"error": "이미지 URL이 제공되지 않았습니다."}), 400
+    if matching_row.empty:
+        return None
 
-    results = predictor.classify_image_url(project_id, publish_iteration_name, image_url)
+    else:
+        return matching_row["에너지(kcal)"].values[0]
+
+
+def predict_image(image_url, foodlog_id):
+    print("Predicting image...")
+    results = predictor.classify_image_url(
+        project_id, publish_iteration_name, image_url
+    )
+
+    detected_tags = []
 
     if results.predictions:
         detected_tags = [prediction.tag_name for prediction in results.predictions]
-        session['detected_tags'] = detected_tags  # 예측 결과를 세션에 저장
-        return jsonify({"message": "이미지 URL이 성공적으로 처리되었습니다.", "tags": detected_tags}), 200
-    else:
-        return jsonify({"message": "태그가 감지되지 않았습니다."}), 404
-
-@app.route('/result', methods=['POST'])
-def post_prediction_result():
-    detected_tags = session.get('detected_tags')
 
     if not detected_tags:
-        return jsonify({"error": "예측된 태그가 없습니다."}), 400
+        return jsonify({"error": "예측된 태그가 없습니다."}), 404
 
-    found = False
-    response_data = {}
-
+    results = []
     for tag in detected_tags:
-        tag = tag.strip()
-        print(f"'{tag}'이 데이터베이스에 있는지 확인 중...")
+        kcal = get_kcal_of_tag_in_db(tag)
+        if kcal is not None:
+            results.append({"kcal": kcal, "tag": tag, "foodlog_id": foodlog_id})
 
-        food_db['음 식 명'] = food_db['음 식 명'].str.strip()
+    # main_server_domain = "http://3.36.148.192:8000/"  # NOTE: 상황에 맞게 수정
+    # url = main_server_domain + "/api/food/image"  # NOTE: 상황에 맞게 수정
+    url = "http://3.36.148.192:8000/api/food/result"
 
-        if tag in food_db['음 식 명'].values:
-            matching_row = food_db[food_db['음 식 명'] == tag]
-            r_value = matching_row['에너지(kcal)'].values[0]
-            response_data = {
-                "tag": tag,
-                "kcal": r_value
-            }
-            found = True
-            break
+    # NOTE: 메인 서버로 결과 전송
+    requests.post(
+        url,
+        json=(
+            results[0]
+            if results
+            else {"kal": None, "tag": None, "foodlog_id": foodlog_id}
+        ),
+    )
 
-    if found:
-        return jsonify(response_data)
-    else:
-        return jsonify({"message": "데이터베이스에서 일치하는 태그를 찾을 수 없습니다."}), 404
 
-if __name__ == '__main__':
-    app.run(debug=True, host='127.0.0.1', port=5001)
-    # mac os에서 airplay receiver이 5000번 포트를 사용중이라 5001로 변경
+@app.route("/", methods=["GET"])
+def home():
+    return "Welcome to the Custom Vision Prediction API!", 200
+
+
+@app.route("/predict", methods=["GET"])
+def get_image_url():
+
+    # NOTE: ai에게 보낼 image_url과 처리 완료 후 이미지 식별용 foodlog_id
+    image_url = request.args.get("image_url")
+    foodlog_id = request.args.get("foodlog_id")
+
+    if not image_url or not foodlog_id:
+        return jsonify({"error": "잘못된 형식의 요청입니다."}), 400
+
+    threading.Thread(target=predict_image, args=(image_url, foodlog_id)).start()
+
+    return (jsonify({"message": "이미지 분석 요청이 정상적으로 처리되었습니다."}), 200)
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host="127.0.0.1", port=5001)
